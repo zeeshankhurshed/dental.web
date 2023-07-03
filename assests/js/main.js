9bb3034c4a8b683fade894a75cf5465d1b6718cd
zeeshankhurshed <109623408+zeeshankhurshed@users.noreply.github.com>
2023-07-03 22:42:56 +0500

    header-sectio

---
+++ assests/js/main.js
@@ -0,0 +1,10 @@
+
+function show(){
+let box =document.getElementById('box');
+    box.style.display = 'block'; 
+}
+
+function hide(){
+    let box =document.getElementById('box');
+        box.style.display = 'none'; 
+    }
