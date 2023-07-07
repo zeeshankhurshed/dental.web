8c846e590bbe60cb257a9da3898b93ece47c9d37
zeeshankhurshed <109623408+zeeshankhurshed@users.noreply.github.com>
2023-07-07 07:47:43 +0500

    full project

--- assests/js/main.js
+++ assests/js/main.js
@@ -8,3 +8,12 @@ function hide(){
     let box =document.getElementById('box');
         box.style.display = 'none'; 
     }
+
+    function show1(){
+        let box1 = document.getElementById('box1');
+        box1.style.display = 'block';
+    }
+    function hide1(){
+        let box1 = document.getElementById('box1');
+        box1.style.display ='none';
+    }
