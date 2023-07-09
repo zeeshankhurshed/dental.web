cf8e689a335cefad7ecae1b297779bb5aedeac02
zeeshankhurshed <109623408+zeeshankhurshed@users.noreply.github.com>
2023-07-09 17:37:59 +0500

    Fix line endings in my-style.css

--- assests/js/main.js
+++ assests/js/main.js
@@ -3,11 +3,11 @@ function show(){
 let box =document.getElementById('box');
     box.style.display = 'block'; 
 }
-
 function hide(){
     let box =document.getElementById('box');
         box.style.display = 'none'; 
     }
+
 
     function show1(){
         let box1 = document.getElementById('box1');
@@ -17,3 +17,33 @@ function hide(){
         let box1 = document.getElementById('box1');
         box1.style.display ='none';
     }
+
+
+    function showtheme(){
+        let theme = document.getElementById('theme');
+        theme.style.display= 'block';
+    }
+        function hidetheme(){
+            let theme = document.getElementById('theme');
+            theme.style.display ='none';
+        }
+    
+
+        function showseller(){
+            let seller = document.getElementById('seller');
+            seller.style.display = 'block';
+        }
+        function hideseller(){
+           let seller = document.getElementById('seller');
+           seller.style.display = 'none';
+        }
+
+
+        function showpanel(){
+            let panel = document.getElementById('panel');
+            panel.style.display ='block';
+        }
+        function hidepanel(){
+            let panel = document.getElementById('panel');
+            panel.style.display = 'none';
+        }
